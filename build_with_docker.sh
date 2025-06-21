3ca14812a2ea9cdca7a8069a079f570740e4107a
Luke Craig <lacraig3@gmail.com>
2025-06-21 16:07:47 -0400

    build_with_docker: drop rust

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker run --rm -v $PWD:/app -w /app  ghcr.io/rehosting/embedded-toolchains_rust:latest /app/package.sh
+docker run --rm -v $PWD:/app -w /app  ghcr.io/rehosting/embedded-toolchains:latest /app/package.sh
